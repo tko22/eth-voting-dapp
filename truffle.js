0798f6aa5ea8614965e7bf952895cf4150e5c7d4
Timothy Ko <timothy.l.ko@gmail.com>
2018-01-12 14:37:05 -0800

    changed to lite-server

--- truffle.js
+++ truffle.js
@@ -1,12 +1,11 @@
-// Allows us to use ES6 in our migrations and tests.
-require('babel-register')
-
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: '127.0.0.1',
+      host: "127.0.0.1",
       port: 7545,
-      network_id: '*' // Match any network id
+      network_id: "*" // Match any network id
     }
   }
-}
+};
