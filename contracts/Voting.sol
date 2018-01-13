d1b955f4b8b9b5e045e4f1d997b2c72e46cd8edc
Timothy Ko <timothy.l.ko@gmail.com>
2018-01-12 17:53:12 -0800

    new functions for smart contract

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -10,12 +10,18 @@ contract Voting {
     // Think of this as a hash table, with the key as a uint and value of the struct Candidate
     mapping (uint => Candidate) candidates;
 
-    function newCandidate(bytes32 name) public returns (uint candidateID) {
+    function addCandidate(bytes32 name) public returns (uint candidateID) {
         // candidateID is the return variable
         candidateID = numCandidates++;
         // Create new Candidate Struct with name and saves it to storage.
         candidates[candidateID] = Candidate(name,0);
     }
 
+    function vote(uint candidateID) public {
+        candidates[candidateID].numOfVotes++;
+    }
 
+    function totalVotes(uint candidateID) view public returns (uint) {
+        return candidates[candidateID].numOfVotes;
+    }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,23 @@
-import "../stylesheets/app.css";
-
 // Import libraries we need.
 import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
-import voting_artifacts from '../../build/contracts/Voting.json'
+import votingArtifacts from '../../build/contracts/Voting.json'
 
-var Voting = contract(voting_artifacts);
-
-
+var Voting = contract(voting_artifacts)
 
 
-$( document ).ready(function() {
+$(document).ready(function() {
   // Is there an injected web3 instance?
   if (typeof web3 !== 'undefined') {
     console.warn("Using web3 detected from external source like Metamask")
     // If there is a web3 instance(in Mist/Metamask), then we use its provider to create our web3object
-    window.web3 = new Web3(web3.currentProvider);
+    window.web3 = new Web3(web3.currentProvider)
   } else {
-    console.warn("No web3 detected. Falling back to http://localhost:7545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    console.warn("No web3 detected. Falling back to http://localhost:7545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask")
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"))
   }
 
-  Voting.setProvider(web3.currentProvider);
-
-});
+  Voting.setProvider(web3.currentProvider)
+})
